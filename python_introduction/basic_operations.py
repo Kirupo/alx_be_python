0132fa88c88ffb14a102428b2b138d012117394c
Kirupo <kirubelg217@gmail.com>
2025-05-22 23:18:07 +0300

    Intial commit for alx_be_python project

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+num1 = 10
+num2 = 5
+
+Addition = num1 + num2
+Subtraction = num1 - num2
+Multiplication = num1 * num2
+
+print("Addition of", num1, "and", num2, "is", Addition)
+print("Subtraction of", num1, "and", num2, "is", Subtraction)
+print("Multiplication of", num1, "and", num2, "is", Multiplication)
+
+
